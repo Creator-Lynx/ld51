c82351f166f537b22804b2fa8c9891ae307bfcc4
BorisDombrowski <32637303+BorisDombrowski@users.noreply.github.com>
2022-10-01 22:08:45 +1000

    Enemy
    реализована база для врага и движущийся к игроку враг

---
+++ Assets/EnemyController/MovingToPlayerEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingToPlayerEnemy : Enemy
+{
+    private Transform Player;
+    public float MoveSpeed = 5f;
+
+    protected override void OnStart()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    protected override void OnUpdate()
+    {
+        if (Player)
+        {
+            var dir = (Player.position - transform.position).normalized;
+            dir = new Vector3(dir.x, 0, dir.z);
+            transform.position += dir * MoveSpeed * Time.deltaTime;
+        }
+    }
+}
