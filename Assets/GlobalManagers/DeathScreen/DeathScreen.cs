de16552916d82dde93473022d6b855db58ad2e8e
Creator-Lynx <1_linx_1@mail.ru>
2022-10-03 20:52:11 +0300

    fixed weapon content исправлены косяки верстки выбора оружия добавлена кнопка в меню смерти добавлено отключение музыки при смерти убрано тестовое оружие

--- Assets/GlobalManagers/DeathScreen/DeathScreen.cs
+++ Assets/GlobalManagers/DeathScreen/DeathScreen.cs
@@ -10,17 +10,24 @@ public class DeathScreen : MonoBehaviour
     public Text Score;
     public Text Kills;
 
+    public AudioSource gameMusic;
+
     public void Init(GameManager gm)
     {
         gameObject.SetActive(true);
         Higscore.text = $"HIGHSCORE: {gm.Highcore}";
         Score.text = $"SCORE: {gm.Score}";
         Kills.text = $"KILLS: {gm.KillCount}";
+        gameMusic.Stop();
     }
 
     public void Restart()
     {
         var scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
