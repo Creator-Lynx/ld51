9eb77017f11e6f3ba0a01edd64749cd01ccb596a
BorisDombrowski <32637303+BorisDombrowski@users.noreply.github.com>
2022-10-03 17:54:52 +1000

    Shield
    Добавлен магический щит

---
+++ Assets/CharController/Scripts/Weapons/Shield/Shield_Lvl1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield_Lvl1 : Weapon
+{
+    private List<Enemy> enemies = new List<Enemy>();
+
+    public override float reloadSpeed => 0.1f;
+    public virtual int damage => 5;
+
+    protected override IEnumerator OnAttack(Vector3 attackDir)
+    {        
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            var en = enemies[i];
+            if (en)
+            {
+                en.SetDamage((int)(damage * _parameters.baseDamage));
+            }
+            else
+            {
+                enemies.Remove(en);
+            }
+        }
+        
+        yield break;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var en = other.GetComponent<Enemy>();
+        //en?.SetDamage((int)(damage * _parameters.baseDamage));
+        if (en)
+        {
+            enemies.Add(en);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var en = other.GetComponent<Enemy>();
+        if (en)
+        {
+            enemies.Remove(en);
+        }
+    }
+}
